fb3870f65ff6e7adfa36a1490a121e20f72d76eb
Violets <violetsamber@gmail.com>
2025-10-11 14:53:22 -0700

    disable permissons, fix title name

--- ui/mission_params.hpp
+++ ui/mission_params.hpp
@@ -345,7 +345,7 @@ class Params {
         title = $STR_PERMISSIONS_PARAM;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 1;
+        default = 0;
     };
     class CleanupVehicles {
         title = $STR_CLEANUP_PARAM;
