475fc73e64f38899ba2e5f1784a03c1f7b8d57f4
Violets <violetsamber@gmail.com>
2025-10-11 14:21:53 -0700

    Enable Permissons system

--- ui/mission_params.hpp
+++ ui/mission_params.hpp
@@ -345,7 +345,7 @@ class Params {
         title = $STR_PERMISSIONS_PARAM;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 0;
+        default = 1;
     };
     class CleanupVehicles {
         title = $STR_CLEANUP_PARAM;
