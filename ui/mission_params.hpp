1957774129c3f71d4a0521601f3de763e417d6fd
Violets <violetsamber@gmail.com>
2025-10-27 13:06:15 -0700

    Reduce aggressiveness from 4 to 2

--- ui/mission_params.hpp
+++ ui/mission_params.hpp
@@ -33,7 +33,7 @@ class Params {
         title = $STR_AGGRESSIVITY_PARAM;
         values[] = {0, 1, 2, 3, 4};
         texts[] = {$STR_AGGRESSIVITY_PARAM0, $STR_AGGRESSIVITY_PARAM1, $STR_AGGRESSIVITY_PARAM2, $STR_AGGRESSIVITY_PARAM3, $STR_AGGRESSIVITY_PARAM4};
-        default = 4;
+        default = 3;
     };
     class AdaptToPlayercount {
         title = $STR_PARAM_ADAPT_TO_PLAYERCOUNT;
