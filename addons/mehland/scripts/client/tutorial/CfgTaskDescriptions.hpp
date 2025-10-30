f0068f579871b6c3de059769c80214d7789e305a
Violets <violetsamber@gmail.com>
2025-10-30 14:47:53 -0700

    fix accidental double quotes

--- addons/mehland/scripts/client/tutorial/CfgTaskDescriptions.hpp
+++ addons/mehland/scripts/client/tutorial/CfgTaskDescriptions.hpp
@@ -1,48 +1,48 @@
 class KPLIB_Tasks_Tutorial_Main {
-    title = "QUOTE($STR_TUTORIAL_TASK_MAIN_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_MAIN_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_MAIN_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_MAIN_DESC);
 };
 class KPLIB_Tasks_Tutorial_Fob {
-    title = "QUOTE($STR_TUTORIAL_TASK_FOB_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_FOB_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_FOB_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_FOB_DESC);
 };
 class KPLIB_Tasks_Tutorial_Fob_01a {
-    title = "QUOTE($STR_TUTORIAL_TASK_FOB_01A_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_FOB_01A_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_FOB_01A_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_FOB_01A_DESC);
 };
 class KPLIB_Tasks_Tutorial_Fob_01b {
-    title = "QUOTE($STR_TUTORIAL_TASK_FOB_01B_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_FOB_01B_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_FOB_01B_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_FOB_01B_DESC);
 };
 class KPLIB_Tasks_Tutorial_Fob_02 {
-    title = "QUOTE($STR_TUTORIAL_TASK_FOB_02_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_FOB_02_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_FOB_02_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_FOB_02_DESC);
 };
 class KPLIB_Tasks_Tutorial_Fob_03 {
-    title = "QUOTE($STR_TUTORIAL_TASK_FOB_03_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_FOB_03_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_FOB_03_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_FOB_03_DESC);
 };
 class KPLIB_Tasks_Tutorial_Sector {
-    title = "QUOTE($STR_TUTORIAL_TASK_SECTOR_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_SECTOR_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_SECTOR_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_SECTOR_DESC);
 };
 class KPLIB_Tasks_Tutorial_Sector_01 {
-    title = "QUOTE($STR_TUTORIAL_TASK_SECTOR_01_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_SECTOR_01_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_SECTOR_01_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_SECTOR_01_DESC);
 };
 class KPLIB_Tasks_Tutorial_Sector_02 {
-    title = "QUOTE($STR_TUTORIAL_TASK_SECTOR_02_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_SECTOR_02_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_SECTOR_02_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_SECTOR_02_DESC);
 };
 class KPLIB_Tasks_Tutorial_Sector_03 {
-    title = "QUOTE($STR_TUTORIAL_TASK_SECTOR_03_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_SECTOR_03_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_SECTOR_03_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_SECTOR_03_DESC);
 };
 class KPLIB_Tasks_Tutorial_Sector_04 {
-    title = "QUOTE($STR_TUTORIAL_TASK_SECTOR_04_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_SECTOR_04_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_SECTOR_04_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_SECTOR_04_DESC);
 };
 class KPLIB_Tasks_Tutorial_Sector_05 {
-    title = "QUOTE($STR_TUTORIAL_TASK_SECTOR_05_TITLE)";
-    description = "QUOTE($STR_TUTORIAL_TASK_SECTOR_05_DESC)";
+    title = QUOTE($STR_TUTORIAL_TASK_SECTOR_05_TITLE);
+    description = QUOTE($STR_TUTORIAL_TASK_SECTOR_05_DESC);
 };
