df641b84651e5c347e2cc21877ea710f67246c4b
Violets <violetsamber@gmail.com>
2025-10-30 19:38:07 -0700

    dont need mehland specfic stuff anymore

--- build/build.sh
+++ build/build.sh
@@ -2,3 +2,4 @@ shopt -s expand_aliases
 source ~/.bash_aliases
 hemtt build
 cp .hemttout/build/addons/kplib_mehland.mehland.pbo ~/.steam/root/steamapps/common/Arma\ 3/MPMissions/kplib_mehland.mehland.pbo
+echo Copied to Arma3 folder
