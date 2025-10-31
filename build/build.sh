7bf71132bb67373af15ff2ffd9e54f383f8e7373
Violets <violetsamber@gmail.com>
2025-10-30 21:00:51 -0700

    fix build script

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 shopt -s expand_aliases
 source ~/.bash_aliases
 hemtt build
-cp .hemttout/build/addons/kplib_mehland.mehland.pbo ~/.steam/root/steamapps/common/Arma\ 3/MPMissions/kplib_mehland.mehland.pbo
+cp .hemttout/build/addons/slklib_mehland.mehland.pbo ~/.steam/root/steamapps/common/Arma\ 3/MPMissions/slklib_mehland.mehland.pbo
 echo Copied to Arma3 folder
