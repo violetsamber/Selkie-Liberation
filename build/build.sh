a6b8c3f7f1308379edd3f1abc97698f572fa7f83
Violets <violetsamber@gmail.com>
2025-10-30 18:42:38 -0700

    Move files again to not make a pbo of just the code and make basic build scripts

---
+++ build/build.sh
@@ -0,0 +1,4 @@
+shopt -s expand_aliases
+source ~/.bash_aliases
+hemtt build
+cp .hemttout/build/addons/kplib_mehland.mehland.pbo ~/.steam/root/steamapps/common/Arma\ 3/MPMissions/kplib_mehland.mehland.pbo
