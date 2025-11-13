56124408e4eb203336bf3115045385d69a762cc9
Violets <violetsamber@gmail.com>
2025-11-13 14:15:59 -0800

    add tests to build script

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,8 @@
 shopt -s expand_aliases
 source ~/.bash_aliases
+
+build/tests.sh
+
 hemtt build
 cp .hemttout/build/addons/slklib_mehland.mehland.pbo ~/.steam/root/steamapps/common/Arma\ 3/MPMissions/slklib_mehland.mehland.pbo
 echo Copied to Arma3 folder
